ef50f3b1216e4f4364e3c7d4481ea3b4e76d45e1
cad-san <cadsan1986@gmail.com>
2013-12-15 19:16:44 +0900

    fix to build with clang

--- scripts/coveralls.sh
+++ scripts/coveralls.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # send coverage report to coveralls
 
+if [ "$CC" = "clang" ]; then
+	exit 0
+fi
 
-if [ "$BUILD_TARGET" = "test" ]; then 
+if [ "$BUILD_TARGET" = "test" ]; then
 	coveralls --root gcov
 fi
